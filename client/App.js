21f3357b810d5499bc8a03439b4f09acec9e9f62
Rexter <yuval654@gmail.com>
2019-07-11 23:16:11 +0300

    Added family search and routing

--- client/App.js
+++ client/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import MainScreen from "./components/MainScreen/MainScreen"
-import {TopBar} from "./components/TopBar/TopBar"
+import { TopBar } from "./components/TopBar/TopBar"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default () => {
     return (
         <div>
             <TopBar />
-            <MainScreen />
+            <Router>
+                <Route exact path="/" component={MainScreen} />
+                <Route exact path="/search/" component={TopBar} />
+            </Router>
         </div>
     )
 }
--- client/components/MainScreen/MainScreen.js
+++ client/components/MainScreen/MainScreen.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./mainScreen.scss";
+import {withRouter} from 'react-router-dom';
 
-export default class MainScreen extends React.PureComponent {
+class MainScreen extends React.PureComponent {
     constructor(props) {
         super(props);
+        this.redirect = this.redirect.bind(this);
+    }
+
+    redirect(path) {
+        return () => this.props.history.push(path);
     }
 
     render() {
@@ -12,10 +18,12 @@ export default class MainScreen extends React.PureComponent {
                 <h1 className={"mainScreen__message_header"}>!ברוכים הבאים לבית מארח</h1>
                 <p className={"mainScreen__message_content"}>האתר שלנו פותח את הלב בתקופות חירום למשפחות מאיזור הדרום<br/>שמעוניינות להתנתק קצת מהמציאות הקשה<br/>וליהנות משקט ומשלווה עד יחלוף זעם </p>
                 <div className={"mainScreen_message_buttons"}>
-                    <button className={"mainScreen__message_buttons_button"}>אפשר להתארח?</button>
+                    <button className={"mainScreen__message_buttons_button"} onClick={this.redirect('search')}>אפשר להתארח?</button>
                     <button className={"mainScreen__message_buttons_button"}>רוצה לארח</button>
                 </div>
             </div>
         </div>
     }
 }
+
+export default withRouter(MainScreen)
